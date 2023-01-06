d252d6eee8ebe0d5ce495d6ef6244137d5f0228e
MO <mo.tech.life20@gmail.com>
2023-01-06 14:55:21 -0500

    used URL search params to get the product id from url; created getProduct function to fetch the product from api

--- front/js/product.js
+++ front/js/product.js
@@ -1,25 +1,18 @@
-// TODO - get specific product from homepage
+// Gets single product id 
+const productUrl = window.location.search;
+const urlParams = new URLSearchParams(productUrl);
+const productID = urlParams.get("_id");
+// Gets single product via product id
+async function getProduct() {
+    const response = await fetch('http://localhost:3000/api/products/' + (productID));
+    const data = await response.json();
+    console.log(data)
+}
+getProduct();
+// TODO - Display the product on the product's page
+
+    // And select the quantity
+    // And select color option
+// TODO - add products to the cart
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//http://127.0.0.1:5500/front/html/product.html?undefined
