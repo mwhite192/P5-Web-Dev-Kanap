9b6470d83c081c928b138e1d7c56626fe838b04a
MO <mo.tech.life20@gmail.com>
2023-01-11 13:24:59 -0500

    added code to loadPage function to enter specific product information on product page

--- front/js/product.js
+++ front/js/product.js
@@ -5,13 +5,24 @@ const productID = urlParams.get("_id");
 // Gets single product via product id
 async function getProduct() {
     const response = await fetch('http://localhost:3000/api/products/' + (productID));
-    const data = await response.json();
-    console.log(data);
+    return response.json(); 
 }
-getProduct();
 // TODO - Display the product on the product's page
+async function loadPage() {
+    const product = await getProduct();
+    const productImage = document.createElement('img');
+    productImage.src = `${product.imageUrl}`;
+    document.getElementsByClassName('item__img')[0].appendChild(productImage);
+    const productTitle = document.getElementById('title');
+    productTitle.textContent = product.name;
+    const productPrice = document.getElementById('price');
+    productPrice.textContent = product.price;
+    const productDescription = document.getElementById('description');
+    productDescription.textContent = product.description;
     // And select the quantity
     // And select color option
+}
+loadPage();
 // TODO - add products to the cart
 
 
