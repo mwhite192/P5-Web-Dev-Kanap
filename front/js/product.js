83c317a642c25e7cf621de3a14275f7f0cb8cdc5
MO <mo.tech.life20@gmail.com>
2023-01-11 14:46:03 -0500

    inserted color option for products in the drop down on products page

--- front/js/product.js
+++ front/js/product.js
@@ -10,17 +10,30 @@ async function getProduct() {
 // TODO - Display the product on the product's page
 async function loadPage() {
     const product = await getProduct();
+    // inserts product image 
     const productImage = document.createElement('img');
     productImage.src = `${product.imageUrl}`;
     document.getElementsByClassName('item__img')[0].appendChild(productImage);
+    // inserts product name 
     const productTitle = document.getElementById('title');
     productTitle.textContent = product.name;
+    // inserts prince of product
     const productPrice = document.getElementById('price');
     productPrice.textContent = product.price;
+    // inserts product description
     const productDescription = document.getElementById('description');
     productDescription.textContent = product.description;
-    // And select the quantity
-    // And select color option
+
+    // below adds color options to product page
+    // loops through color array and inserts product color options
+    for (let i = 0; i < product.colors.length; i++) {
+      const colorChoice = document.getElementById("colors");
+      const option = document.createElement("option");
+      option.value = product.colors[i];
+      option.text = product.colors[i];
+      colorChoice.add(option);
+    }
+    // And select the quantity  
 }
 loadPage();
 // TODO - add products to the cart
