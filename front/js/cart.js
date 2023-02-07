95c6d2630fbfc5102d39066a53ea77ce03cd1e56
MO <mo.tech.life20@gmail.com>
2023-02-07 18:44:13 -0500

    working on function and event listener to update quantity

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,9 +4,12 @@ function getCartFromStorage(){
 }
 // Gets the cart from local storage
 const customerCart = getCartFromStorage();
+// -----------------------------------------------------
 
 // Gets the itemID from each item in the cart array
 const customerCartID = customerCart.map(item => item.itemID);
+//------------------------------------------------------
+
 // Fetches the product info for each item in the cart array
 Promise.all(customerCartID.map(id => fetch(`http://localhost:3000/api/products/${id}`).then((data) => data.json())))
 .then((data) =>{
@@ -24,6 +27,7 @@ Promise.all(customerCartID.map(id => fetch(`http://localhost:3000/api/products/$
   }
   insertCartItems(customerOrder);
 });
+// -----------------------------------------------------
 // Inserts cart items into cart page
 function insertCartItems(customerOrder){
   // Iterates over the cart array 
@@ -57,13 +61,18 @@ function insertCartItems(customerOrder){
     </div>`;
   }
 }
+// -----------------------------------------------------
 
 // Gets the sum of the quantity of items in the cart
-const cartQty = customerCart.map(qty => qty.itemQty).map(Number).reduce((sum, index) => sum + index, 0);
-// Gets the total quantity element on the cart page
-const totalQtyHolder = document.getElementById("totalQuantity");
-// Inserts the sum of items in the cart into the total quantity element
-totalQtyHolder.textContent = cartQty;
+function getCartQty(){
+  const cartQty = customerCart.map(qty => qty.itemQty).map(Number).reduce((sum, index) => sum + index, 0);
+  // Gets the total quantity element on the cart page
+  const totalQtyHolder = document.getElementById("totalQuantity");
+  // Inserts the sum of items in the cart into the total quantity element
+  totalQtyHolder.textContent = cartQty;
+}
+getCartQty();
+// -----------------------------------------------------
 
 // Gets the sum of the price of items in the cart
 Promise.all(customerCartID.map(id => fetch(`http://localhost:3000/api/products/${id}`).then((data) => data.json())))
@@ -90,9 +99,37 @@ Promise.all(customerCartID.map(id => fetch(`http://localhost:3000/api/products/$
   // Inserts the total price into the total price element
   totalHolder.textContent = cartTotal;
 });
+// -----------------------------------------------------
 
-
-
+// updates the cart quantity and total price when the quantity of an item is changed
+function updateCart(){
+  // Gets the cart item elements on the cart page
+  const cartItems = document.getElementsByClassName("cart__items");
+  // Iterates over the cart item elements
+  for (let i = 0; i < cartItems.length; i++){
+    // Gets the quantity of each item in the cart
+    const itemQty = cartItems[i].getElementsByClassName("itemQuantity")[0].value;
+    // Gets the itemID of each item in the cart
+    const itemID = cartItems[i].dataset.id;
+    // Gets the itemColor of each item in the cart
+    const itemColor = cartItems[i].dataset.color;
+    // Iterates over the cart array
+    for (let j = 0; j < customerCart.length; j++){
+      // If the itemID and itemColor of the current cart item element matches the itemID and itemColor of the current cart array element
+      if (itemID === customerCart[j].itemID && itemColor === customerCart[j].itemColor){
+        // Update the itemQty of the current cart array element
+        customerCart[j].itemQty = itemQty;
+      }
+    }
+  }
+  // Updates the cart in local storage
+  localStorage.setItem('shoppingCart', JSON.stringify(customerCart));
+}
+// event listener for the quantity of each item in the cart
+document.addEventListener('change', updateCart);
+// for (let i = 0; i < itemQty.length; i++){
+//   itemQty[i].addEventListener('change', updateCart);
+//}
 
 
 
